a0d29484569037bbcab209f3a5553cfc7ce7010b
Steven Hirschmann <StevenHirschmann14@gmail.com>
2018-02-15 10:13:33 +0000

    -- Added default FullName method to the IObject interface.

--- src/main/java/Core/Parser/Models/IObject.java
+++ src/main/java/Core/Parser/Models/IObject.java
@@ -5,6 +5,10 @@ public interface IObject {
     void Parent(IObject parent);
     String Name();
     void Name(String value);
+    default String FullName(){
+        IObject parent = Parent();
+        return (parent != null) ? parent.FullName() + "." + Name() : Name();
+    }
     String[] Body();
     void Body(String[] body);
 }
