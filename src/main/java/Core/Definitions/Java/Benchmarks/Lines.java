45fc2db70a079efcea33fb6af6916c5e83627163
Steven Hirschmann <StevenHirschmann14@gmail.com>
2018-03-01 10:43:09 +0000

    -- Added File wide entry. -- Refactored analyzeBody to use Object constructor on entry.

--- src/main/java/Core/Definitions/Java/Benchmarks/Lines.java
+++ src/main/java/Core/Definitions/Java/Benchmarks/Lines.java
@@ -3,8 +3,8 @@ package Core.Definitions.Java.Benchmarks;
 import Core.Entry;
 import Core.Parser.Models.Class;
 import Core.Parser.Models.File;
-import Core.Parser.Models.IObject;
 import Core.Parser.Models.Method;
+import Core.Parser.Models.Object;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +14,7 @@ public class Lines extends Core.Analyzer.Benchmarks.LinesBase {
     @Override
     public Entry[] run(File model) {
         List<Entry> entries = new ArrayList<>();
+        entries.add(analyzeBody(model));
         for (Class c: model.Classes) {
             for (Method m : c.Methods){
                 entries.add(analyzeBody(m));
@@ -22,11 +23,8 @@ public class Lines extends Core.Analyzer.Benchmarks.LinesBase {
         return entries.toArray(new Entry[entries.size()]);
     }
 
-    private Entry analyzeBody(IObject object){
-        Entry entry = new Entry();
-        entry.Name = object.FullName();
-        entry.Type = this.Type();
-        entry.Path = object.Path();
+    private Entry analyzeBody(Object object){
+        Entry entry = new Entry(object, this.Type());
         entry.Value = object.Body().length;
         return entry;
     }
