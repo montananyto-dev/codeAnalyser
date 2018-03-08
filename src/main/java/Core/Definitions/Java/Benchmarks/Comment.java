40cfd8dc341125addc131e8419346b9ece7b8a35
Deep <34748292+Awaddd@users.noreply.github.com>
2018-03-08 10:30:45 +0000

    Attempted to count comments.

--- src/main/java/Core/Definitions/Java/Benchmarks/Comment.java
+++ src/main/java/Core/Definitions/Java/Benchmarks/Comment.java
@@ -2,13 +2,44 @@ package Core.Definitions.Java.Benchmarks;
 
 import Core.Entry;
 import Core.Parser.Models.File;
+import Core.Parser.Models.IObject;
+import Core.Parser.Models.Method;
+import Core.Parser.Models.Field;
 
+import java.util.ArrayList;
+import java.util.List;
+//import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Comment extends Core.Analyzer.Benchmarks.CommentBase {
 
     @Override
     public Entry[] run(File model) {
+ 
+        List<Entry> entries = new ArrayList<>();
+        
+        int sum = 0;
+        
+        for (Core.Parser.Models.Class c: model.Classes) {
+            
+            for (Method m : c.Methods){
+                sum += model.Comments().length;
+                entries.add(countComments(sum));
+            }
 
-        return new Entry[0];
+            for (Field f : c.Fields){
+                sum += model.Comments().length;
+                entries.add(countComments(sum));
+            }
+            
+        }
+//        return entries.toArray(new Entry[entries.size()]);
+            return new Entry[sum];
     }
+
+    private Entry countComments(int sum){
+        Entry entry = new Entry();
+        entry.Value = sum;
+        return entry;
+    }
+    
 }
